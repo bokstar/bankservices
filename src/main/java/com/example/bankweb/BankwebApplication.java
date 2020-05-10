fa350f38b97984b302382747e7c962ab94055b9d
JaeBokShin <tlswoqhr12@naver.com>
2020-05-10 16:55:28 +0900

    first commit

---
+++ src/main/java/com/example/bankweb/BankwebApplication.java
@@ -0,0 +1,13 @@
+package com.example.bankweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankwebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankwebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bankweb/BankwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bankweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankwebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
