42398be0bed707232162196e5276626e6afe8b97
JaeBokShin <tlswoqhr12@naver.com>
2020-05-11 12:57:26 +0900

    add serviceImp

--- src/main/java/com/example/bankweb/service/serivceImp/TransactionServiceImp.java
+++ src/main/java/com/example/bankweb/service/serivceImp/TransactionServiceImp.java
@@ -1,69 +1,151 @@
 package com.example.bankweb.service.serivceImp;
 
+import com.example.bankweb.dao.*;
 import com.example.bankweb.domain.*;
 import com.example.bankweb.service.TransactionService;
+import com.example.bankweb.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.security.Principal;
+import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
+@Service
 public class TransactionServiceImp implements TransactionService {
+
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private PrimaryTransactionDao primaryTransactionDao;
+
+    @Autowired
+    private SavingsTransactionDao savingsTransactionDao;
+
+    @Autowired
+    private PrimaryAccountDao primaryAccountDao;
+
+    @Autowired
+    private SavingsAccountDao savingsAccountDao;
+
+    @Autowired
+    private RecipientDao recipientDao;
+
+
     @Override
     public List<PrimaryTransaction> findPrimaryTransactionList(String username) {
-        return null;
+        User user = userService.findByUsername(username);
+        List<PrimaryTransaction> primaryTransactionList = user.getPrimaryAccount().getPrimaryTransactionList();
+        return primaryTransactionList;
     }
 
     @Override
     public List<SavingsTransaction> findSavingsTransactionList(String username) {
-        return null;
+        User user = userService.findByUsername(username);
+        List<SavingsTransaction> savingsTransactionList = user.getSavingsAccount().getSavingsTransactionList();
+        return savingsTransactionList;
     }
 
     @Override
     public void savePrimaryDepositTransaction(PrimaryTransaction primaryTransaction) {
-
+        primaryTransactionDao.save(primaryTransaction);
     }
 
     @Override
     public void saveSavingsDepositTransaction(SavingsTransaction savingsTransaction) {
-
+        savingsTransactionDao.save(savingsTransaction);
     }
 
     @Override
     public void savePrimaryWithdrawTransaction(PrimaryTransaction primaryTransaction) {
-
+        primaryTransactionDao.save(primaryTransaction);
     }
 
     @Override
     public void saveSavingsWithdrawTransaction(SavingsTransaction savingsTransaction) {
 
+        savingsTransactionDao.save(savingsTransaction);
     }
 
+
+    // 계좌 사이의 돈 전달로직
+    // 나중에 음수는 불가능하게 수정
     @Override
     public void betweenAccountsTransfer(String transferFrom, String transferTo, String amount, PrimaryAccount primaryAccount, SavingsAccount savingsAccount) throws Exception {
+        if (transferFrom.equalsIgnoreCase("Primary") && transferTo.equalsIgnoreCase("Savings")) {
+            primaryAccount.setAccountBalance(primaryAccount.getAccountBalance().subtract(new BigDecimal(amount)));
+            savingsAccount.setAccountBalance(savingsAccount.getAccountBalance().add(new BigDecimal(amount)));
+            primaryAccountDao.save(primaryAccount);
+            savingsAccountDao.save(savingsAccount);
+
+            Date date = new Date();
+
+            PrimaryTransaction primaryTransaction = new PrimaryTransaction(date, "Between account transfer from " + transferFrom + " to " + transferTo, "Account", "Finished", Double.parseDouble(amount), primaryAccount.getAccountBalance(), primaryAccount);
+            primaryTransactionDao.save(primaryTransaction);
+        } else if (transferFrom.equalsIgnoreCase("Savings") && transferTo.equalsIgnoreCase("Primary")) {
+            primaryAccount.setAccountBalance(primaryAccount.getAccountBalance().add(new BigDecimal(amount)));
+            savingsAccount.setAccountBalance(savingsAccount.getAccountBalance().subtract(new BigDecimal(amount)));
+            primaryAccountDao.save(primaryAccount);
+            savingsAccountDao.save(savingsAccount);
+
+            Date date = new Date();
+
+            SavingsTransaction savingsTransaction = new SavingsTransaction(date, "Between account transfer from " + transferFrom + " to " + transferTo, "Transfer", "Finished", Double.parseDouble(amount), savingsAccount.getAccountBalance(), savingsAccount);
+            savingsTransactionDao.save(savingsTransaction);
+        } else {
+            throw new Exception("Invalid Transfer");
+        }
 
     }
 
     @Override
     public List<Recipient> findRecipientList(Principal principal) {
-        return null;
+        String username = principal.getName();
+        List<Recipient> recipientList = recipientDao.findAll().stream()
+                .filter(recipient -> username.equals(recipient.getUser().getUsername()))
+                .collect(Collectors.toList());
+
+        return recipientList;
+
     }
 
     @Override
     public Recipient saveRecipient(Recipient recipient) {
-        return null;
+    return recipientDao.save(recipient);
     }
 
     @Override
     public Recipient findRecipientByName(String recipientName) {
-        return null;
+    return recipientDao.findByName(recipientName);
     }
 
     @Override
     public void deleteRecipientByName(String recipientName) {
-
+        recipientDao.deleteByName(recipientName);
     }
 
     @Override
     public void toSomeoneElseTransfer(Recipient recipient, String accountType, String amount, PrimaryAccount primaryAccount, SavingsAccount savingsAccount) {
+        if (accountType.equalsIgnoreCase("Primary")) {
+            primaryAccount.setAccountBalance(primaryAccount.getAccountBalance().subtract(new BigDecimal(amount)));
+            primaryAccountDao.save(primaryAccount);
 
+            Date date = new Date();
+
+            PrimaryTransaction primaryTransaction = new PrimaryTransaction(date, "Transfer to recipient " + recipient.getName(), "Transfer", "Finished", Double.parseDouble(amount), primaryAccount.getAccountBalance(), primaryAccount);
+            primaryTransactionDao.save(primaryTransaction);
+        } else if (accountType.equalsIgnoreCase("Savings")) {
+            savingsAccount.setAccountBalance(savingsAccount.getAccountBalance().subtract(new BigDecimal(amount)));
+            savingsAccountDao.save(savingsAccount);
+
+            Date date = new Date();
+
+            SavingsTransaction savingsTransaction = new SavingsTransaction(date, "Transfer to recipient " + recipient.getName(), "Transfer", "Finished", Double.parseDouble(amount), savingsAccount.getAccountBalance(), savingsAccount);
+            savingsTransactionDao.save(savingsTransaction);
+        }
     }
 }
--- src/main/java/com/example/bankweb/service/serivceImp/UserServiceImp.java
+++ src/main/java/com/example/bankweb/service/serivceImp/UserServiceImp.java
@@ -1,66 +1,132 @@
 package com.example.bankweb.service.serivceImp;
 
+import com.example.bankweb.dao.RoleDao;
+import com.example.bankweb.dao.UserDao;
 import com.example.bankweb.domain.User;
 import com.example.bankweb.domain.security.UserRole;
+import com.example.bankweb.service.AccountService;
 import com.example.bankweb.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Set;
 
+
+@Service
+@Transactional
 public class UserServiceImp implements UserService {
+
+
+    private static final Logger LOG = LoggerFactory.getLogger(UserService.class);
+
+    @Autowired
+    private UserDao userDao;
+
+    @Autowired
+    private RoleDao roleDao;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @Autowired
+    private AccountService accountService;
+
 
     @Override
     public User findByUsername(String username) {
-        return null;
+        return userDao.findByUsername(username);
     }
 
     @Override
     public User findByEmail(String email) {
-        return null;
+        return userDao.findByEmail(email);
     }
 
+    // 회원가입 할 때 아이디가 존재하는지 확인
     @Override
     public boolean checkUserExists(String username, String email) {
-        return false;
+        if (checkUsernameExists(username) || checkEmailExists(username)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     @Override
     public boolean checkUsernameExists(String username) {
+        if (null != findByUsername(username)) {
+            return true;
+        }
         return false;
     }
 
     @Override
     public boolean checkEmailExists(String email) {
+        if (null != findByEmail(email)) {
+            return true;
+        }
         return false;
     }
 
     @Override
     public void save(User user) {
-
+        userDao.save(user);
     }
 
     @Override
     public User createUser(User user, Set<UserRole> userRoles) {
-        return null;
+        User localUser = userDao.findByUsername(user.getUsername());
+
+        if (localUser != null) {
+            LOG.info("User with username {} already exist. Nothing will be done. ", user.getUsername());
+        } else {
+            String encryptedPassword = passwordEncoder.encode(user.getPassword());
+            user.setPassword(encryptedPassword);
+
+            for (UserRole ur : userRoles) {
+                roleDao.save(ur.getRole());
+            }
+
+            user.getUserRoles().addAll(userRoles);
+
+            user.setPrimaryAccount(accountService.createPrimaryAccount());
+            user.setSavingsAccount(accountService.createSavingsAccount());
+
+            localUser = userDao.save(user);
+        }
+
+        return localUser;
+
     }
 
     @Override
     public User saveUser(User user) {
-        return null;
+        return userDao.save(user);
     }
 
     @Override
     public List<User> findUserList() {
-        return null;
+        return userDao.findAll();
     }
 
     @Override
     public void enableUser(String username) {
-
+        User user = findByUsername(username);
+        user.setEnabled(true);
+        userDao.save(user);
     }
 
     @Override
     public void disableUser(String username) {
-
+        User user = findByUsername(username);
+        user.setEnabled(false);
+        System.out.println(user.isEnabled());
+        userDao.save(user);
+        System.out.println(username + " is disabled.");
     }
 }
